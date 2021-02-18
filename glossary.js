fd56cdebf9d3f61a6fd15125dea61318267f3624
Topaz <topaz.git@lt3.us>
2021-02-18 02:13:02 -0500

    add speedrunning glossary and improve glossary code

--- glossary.js
+++ glossary.js
@@ -1,3 +1,5 @@
+(function(){
+
 function ce(tagname, attr, children) {
   var el = document.createElement(tagname);
   if (attr) {
@@ -50,35 +52,47 @@ Array.prototype.splicejoin = function splicejoin(sep) {
 })();
 
 
+function init_error(message) {
+  dom_clear(document.body);
+  document.body.appendChild(ce("p",null,[ce("strong",null,"Error: "),""+message]));
+  console.error(message);
+}
+
 var load_counter = 0;
-function done(){
+function load_start(){
+  load_counter++;
+}
+function load_done(){
   load_counter--;
   if (load_counter == 0) {
     ready();
-    done = console.error;
+    load_done = console.error;
   }
 }
 
 var glossary_yaml = null;
 
-load_counter++;
+load_start();
 (function(){
   var script = document.createElement("script");
-  script.onload = done;
+  script.addEventListener('load', load_done);
+  script.addEventListener('error', function(){init_error("Failed to load js-yaml library.")});
   script.src = "https://cdnjs.cloudflare.com/ajax/libs/js-yaml/3.13.1/js-yaml.min.js";
   document.head.appendChild(script);
 })();
 
-load_counter++;
-document.addEventListener("DOMContentReady", done);
+load_start();
+document.addEventListener("DOMContentLoaded", load_done);
 
+load_start();
 (function(){
   var req = new XMLHttpRequest();
   req.open("GET", "glossary.yaml");
   req.addEventListener("load", function(e){
     glossary_yaml = req.responseText;
-    done();
+    load_done();
   });
+  req.addEventListener('error', function(){init_error("Failed to load glossary yaml.")});
   req.send();
 })();
 
@@ -88,23 +102,31 @@ function ready() {
   try {
     data = jsyaml.load(glossary_yaml);
   } catch(err) {
-    console.error(err);
+    init_error(err);
     return;
   }
 
   document.head.appendChild(ce("title",{},data.title||"Glossary"));
   document.body.appendChild(glossary_render(data))
 
+  // collect all linkable dfns
   var dfn_by_id = {};
   Array.from(document.querySelectorAll("dfn > .anchor[id]")).forEach(el=>{
     if (!(el.id in dfn_by_id)) dfn_by_id[el.id] = [];
     dfn_by_id[el.id].push(el);
   });
+
+  // check that all dfns are unique
   Object.keys(dfn_by_id).forEach(id=>{
     if (dfn_by_id[id].length>1) dfn_by_id[id].forEach(el=>{
       el.parentNode.classList.add("error");
       el.parentNode.title = "Duplicate definition!";
     });
+  });
+
+  // check that all internal links point to existing dfns
+  Array.from(document.querySelectorAll("a[href^=\"#\"]")).forEach(el=>{
+    if (!(el.getAttribute("href").substr(1) in dfn_by_id)) el.classList.add("unknown-href");
   });
 
   var hash;
@@ -127,23 +149,46 @@ function ready() {
   if (hash) location.hash = hash;
 }
 
+var known_keys = {synonyms:1,definition:1,examples:1,more_info:1,see_also:1};
 function glossary_render(data) {
   return ce("div",{className:"glossary"},[
+    !data.title ? [] : ce("h1", null, data.title),
     ce("dl", {className:"words"}, Object.keys(data.words).sort((a,b)=>a.localeCompare(b)).map(word => {
-      var info = data.words[word];
+      var info = data.words[word] || {};
+      var unknown_keys = Object.keys(info).filter(k => !(k in known_keys));
+      if (unknown_keys.length) console.error("Unknown keys for word \"%s\": %o", word, unknown_keys);
       return ce("div", {className:"word"}, [
         ce("dt", {className:"word-terms"}, [dfn_render(word)].concat(
-          !info.synonyms   ? [] : ce("span",{className:"word-synonyms"}, info.synonyms.map(syn => dfn_render(syn)).splicejoin(", ")),
+          !info.synonyms ? [] : ce("span",{className:"word-synonyms"},  info.synonyms .map(syn       => dfn_render(syn)).splicejoin(", ")),
         )),
-        !info.definition ? [] : ce("dd", {className:"word-definition"}, info.definition),
-        !info.examples   ? [] : ce("dd", {className:"word-examples"}, info.examples.map(example => ce("a", {href:example.url}, example.title)).splicejoin(", ")),
-        !info.related    ? [] : ce("dd", {className:"word-related"},  info.related .map(related => ce("a", {href:related.url}, related.title)).splicejoin(", ")),
-        !info.see_also   ? [] : ce("dd", {className:"word-seealso"},  info.see_also.map(seealso => ce("a", {href:"#"+seealso}, seealso)).splicejoin(", ")),
+        !info.definition ? [] : ce("dd", {className:"word-definition"}, inline_render(info.definition, data)),
+        !info.examples   ? [] : ce("dd", {className:"word-examples"},   info.examples .map(example   => ce("a", {href:example.url}, example.title)).splicejoin(", ")),
+        !info.more_info  ? [] : ce("dd", {className:"word-moreinfo"},   info.more_info.map(more_info => ce("a", {href:more_info.url}, more_info.title)).splicejoin(", ")),
+        !info.see_also   ? [] : ce("dd", {className:"word-seealso"},    info.see_also .map(seealso   => ce("a", {href:"#"+word_anchor(seealso)}, seealso)).splicejoin(", ")),
       ]);
     })),
   ]);
 }
 
-function dfn_render(word) {
-  return ce("dfn",{},[ce("div",{id:word,className:"anchor"}),ce("a",{href:"#"+word},word)]);
+function word_anchor(word) {
+  return word.toLowerCase().replace(/\s/g,'_');
 }
+
+function dfn_render(word) {
+  return ce("dfn",{},[ce("div",{id:word_anchor(word),className:"anchor"}),ce("a",{href:"#"+word_anchor(word)},word)]);
+}
+
+function inline_render(markup) {
+  var parts = markup.split(/\[([^\[\]]+)\]/g);
+  var dom = [];
+  for (var i=0; i<parts.length; i++) {
+    if (i % 2 == 0) {
+      if (parts[i].length) dom.push(parts[i]);
+    } else {
+      dom.push(ce("a", {href:"#"+word_anchor(parts[i])}, parts[i]));
+    }
+  }
+  return dom;
+}
+
+})();
